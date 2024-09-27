64943f457557df4b5dd912b4d12b3b4ee98c15f4
akay <kenesakai666@gmail.com>
2024-09-27 09:04:44 +0500

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer" id="footer">
+        <div className="footer-content">
+          <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consectetur illo distinctio quia nihil pariatur est voluptatibus
+              consequatur inventore sapiente incidunt quos eveniet magni iure,
+              totam asperiores culpa minus nisi hic atque commodi id eius maxime
+              animi. Nulla illo sunt inventore placeat nisi consequuntur.
+            </p>
+            <div className="footer-social-icons">
+              <img src={assets.facebook_icon} alt="" />
+              <img src={assets.twitter_icon} alt="" />
+              <img src={assets.linkedin_icon} alt="" />
+            </div>
+          </div>
+          <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Delivery</li>
+              <li>Privacy policy</li>
+            </ul>
+          </div>
+          <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+              <li>+7-707-884-12-36</li>
+              <li>kenesakai666@gmail.com</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">
+          Copyright 2024 © Tomato.com - All Right Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
